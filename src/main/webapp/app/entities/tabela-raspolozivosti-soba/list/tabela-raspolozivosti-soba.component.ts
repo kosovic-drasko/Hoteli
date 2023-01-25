f413df36c2fb600c16f9e75869ea916457fcff89
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2023-01-25 06:43:14 +0100

    init

--- src/main/webapp/app/entities/tabela-raspolozivosti-soba/list/tabela-raspolozivosti-soba.component.ts
+++ src/main/webapp/app/entities/tabela-raspolozivosti-soba/list/tabela-raspolozivosti-soba.component.ts
@@ -6,8 +6,8 @@ import { CalendarEvent, CalendarView } from 'angular-calendar';
 
 @Component({
   selector: 'jhi-tabela-raspolozivosti-soba',
-  styleUrls: ['/jhi-tabela-raspolozivosti.scss'],
-  templateUrl: 'jhi-tabela-raspolozivosti-soba.component.html',
+  styleUrls: ['/tebela-raspolozivosti.scss'],
+  templateUrl: 'tabela-raspolozivosti-soba.component.html',
 })
 export class TabelaRaspolozivostiSobaComponent {
   viewDate: Date = new Date();
